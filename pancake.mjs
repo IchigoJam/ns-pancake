3d37f46482726afd6c3410764276cdbd05069cab
Taisuke Fukuno <fukuno@jig.jp>
2021-04-07 07:23:22 +0900

    add pancake_palette.mjs

--- pancake.mjs
+++ pancake.mjs
@@ -1,4 +1,5 @@
 import { setUI, getContext } from "./canvasutil.mjs";
+import { PANCAKE_PALETTE } from "./PANCAKE_PALETTE.mjs";
 
 const dw = 80;
 const dh = 45;
@@ -120,25 +121,6 @@ const init = (canvas) => {
 		
 };
 
-const PC_PALETTE = [
-	[ 0, 0, 0 ],
-	[ 255, 255, 255 ],
-	[ 227, 27, 41 ],
-	[ 255, 104, 139 ],
-	[ 242, 126, 48 ],
-	[ 255, 203, 61 ],
-	[ 255, 222, 169 ],
-	[ 107, 74, 43 ],
-	[ 145, 202, 24 ],
-	[ 28, 75, 55 ],
-	[ 67, 175, 215 ],
-	[ 38, 74, 208 ],
-	[ 26, 36, 102 ],
-	[ 99, 55, 187 ],
-	[ 178, 63, 171 ],
-	[ 204, 204, 204 ]
-];
-
 class NSPanCake extends HTMLElement {
 	constructor() {
 		super();
@@ -166,7 +148,7 @@ class NSPanCake extends HTMLElement {
 			g.fillRect(0, 0, g.cw, g.ch);
 	//		g.setColor(200, 200, 200);
 	//		g.fillRect(ox - 1, oy - 1, tw * dw + 2, tw * dh + 2);
-			const col = PC_PALETTE;
+			const col = PANCAKE_PALETTE;
 			const dots = this.dots;
 			for (let i = 0; i < dh; i++) {
 				for (let j = 0; j < dw; j++) {
